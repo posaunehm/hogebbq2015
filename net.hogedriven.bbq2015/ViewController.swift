60767e0c8e146af8387dfeef1c2ad760386f573b
Hiroshi Maekawa <maekawa@guildworks.jp>
2015-07-20 14:44:57 +0900

    initial commit

---
+++ net.hogedriven.bbq2015/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  net.hogedriven.bbq2015
+//
+//  Created by 前川博志 on 2015/07/20.
+//  Copyright (c) 2015年 前川博志. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ net.hogedriven.bbq2015Tests/net_hogedriven_bbq2015Tests.swift
@@ -0,0 +1,36 @@
+//
+//  net_hogedriven_bbq2015Tests.swift
+//  net.hogedriven.bbq2015Tests
+//
+//  Created by 前川博志 on 2015/07/20.
+//  Copyright (c) 2015年 前川博志. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class net_hogedriven_bbq2015Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
